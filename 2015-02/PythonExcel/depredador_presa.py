642fd4ffd0279a6be8e601aa3fb4d3363ae34bb7
Fran Navarro <fjnavarrob@gmail.com>
2015-11-29 17:42:56 -0500

    Updated to xlwings v 0.5

--- 2015-02/PythonExcel/depredador_presa.py
+++ 2015-02/PythonExcel/depredador_presa.py
@@ -10,62 +10,74 @@ http://pybonacci.org/2015/01/05/ecuaciones-de-lotka-volterra-modelo-presa-depred
 
 """
 
+from __future__ import division
+import os
+import sys
 import numpy as np
+from xlwings import Workbook, Range, Chart
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from xlwings import Workbook, Range, Chart
 
-wb = Workbook.caller()
+def main():
+	wb = Workbook.caller()
 
-def df_dt(x, t, a, b, c, d):
-    """Función del sistema en forma canónica"""
-    dx = (a * x[0]-r*x[0]**2) - b * x[0] * x[1]
-    dy = - c * x[1] + d * x[0] * x[1]
-    return np.array([dx, dy])
+	def df_dt(x, t, a, b, c, d):
+		"""Función del sistema en forma canónica"""
+		dx = (a * x[0]-r*x[0]**2) - b * x[0] * x[1]
+		dy = - c * x[1] + d * x[0] * x[1]
+		return np.array([dx, dy])
 
-if Range('W2').value.lower() == 'yes':
-    animate = True
-else:
-    animate = False
+	if Range('W2').value.lower() == 'yes':
+		animate = True
+	else:
+		animate = False
 
-# Parámetros
-a = Range('constante_a').value
-b = Range('X4').value
-c = Range('X5').value
-d = Range('X6').value
-r = Range('X7').value
+	# Parámetros
+	a = Range('constante_a').value
+	b = Range('X4').value
+	c = Range('X5').value
+	d = Range('X6').value
+	r = Range('X7').value
 
-# Condiciones iniciales
-x0 = Range('X13').value   # Presas
-y0 = Range('X14').value    # Depredadores
-conds_iniciales = np.array([x0, y0])
+	# Condiciones iniciales
+	x0 = Range('X13').value   # Presas
+	y0 = Range('X14').value    # Depredadores
+	conds_iniciales = np.array([x0, y0])
 
-# Condiciones para integración
-tf = Range('X10').value
-N = 50
-N = Range('X9').value
-N = int(N)
-t = np.linspace(0, tf, N)
-
-
-solucion = odeint(df_dt, conds_iniciales, t, args=(a, b, c, d))
-#representaciones graficas
-#plt.plot(t, solucion[:, 0], label='presa')
-#plt.plot(t, solucion[:, 1], label='depredador')
+	# Condiciones para integración
+	tf = Range('X10').value
+	N = 50
+	N = Range('X9').value
+	N = int(N)
+	t = np.linspace(0, tf, N)
 
 
+	solucion = odeint(df_dt, conds_iniciales, t, args=(a, b, c, d))
+	#representaciones graficas
+	#plt.plot(t, solucion[:, 0], label='presa')
+	#plt.plot(t, solucion[:, 1], label='depredador')
 
-nn = np.linspace(0, N, N)
-if animate:
-    for i in range(1,N):
-        #nn[i] = i
-        #Range('D1').value = np.vstack(nn)
-        Range((i+1,20)).value = t[i-1]
-        Range((i+1,21)).value = solucion[i-1, 0]
-        Range((i+1,22)).value = solucion[i-1, 1]
-        wb.xl_app.Application.ScreenUpdating = True
-else:
-     Range('T2').value = np.vstack(t)
-     Range('U2').value = np.vstack(solucion[:, 0])
-     Range('V2').value = np.vstack(solucion[:, 1])
 
+
+	nn = np.linspace(0, N, N)
+	if animate:
+		for i in range(1,N):
+			#nn[i] = i
+			#Range('D1').value = np.vstack(nn)
+			Range((i+1,20)).value = t[i-1]
+			Range((i+1,21)).value = solucion[i-1, 0]
+			Range((i+1,22)).value = solucion[i-1, 1]
+			wb.xl_app.Application.ScreenUpdating = True
+	else:
+		 Range('T2').value = np.vstack(t)
+		 Range('U2').value = np.vstack(solucion[:, 0])
+		 Range('V2').value = np.vstack(solucion[:, 1])
+
+if __name__ == '__main__':
+    if not hasattr(sys, 'frozen'):
+        # The next two lines are here to run the example from Python
+        # Ignore them when called in the frozen/standalone version
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'depredador.xlsm'))
+        Workbook.set_mock_caller(path)
+    main()
